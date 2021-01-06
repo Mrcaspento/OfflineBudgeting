d5d1d5c6eaee3a05add6f77bb06e4ffbf222fb03
Caspento <Mrcaspento@gmail.com>
2021-01-06 18:32:00 -0500

    adding the cache event

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -6,13 +6,24 @@ const cacheAssets = [
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     '/index.js',
-    '/style.css'
+    '/style.css',
+    'service_worker.js'
 ]
 
 
 //Call Install Event
 self.addEventListener('install', (e) => {
     console.log("Service Worker: Installed");
+
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log('Service Worker: Caching Files');
+                cache.addAll(cacheAssets);
+            })
+            .then(() => self.skipWaiting())
+    );
 });
 
 //Call Active Event
