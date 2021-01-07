9a0460565754369f19948397a88bb07ca5e09937
Caspento <Mrcaspento@gmail.com>
2021-01-06 21:33:22 -0500

    link manifest still need to fix the links to icons and testing the offline cacheing

--- public/service_worker.js
+++ public/service_worker.js
@@ -18,8 +18,8 @@ self.addEventListener('install', (e) => {
     console.log("Service Worker: Installed");
 
     e.waitUntil(
-      caches
-        .open(CACHE_NAME)
+        caches
+            .open(CACHE_NAME)
             .then(cache => {
                 console.log('Service Worker: Caching Files')
                 return cache.addAll(CACHE_ASSETS);
@@ -33,15 +33,45 @@ self.addEventListener('activate', (e) => {
     console.log("Service Worker: Activated");
     //Remove unwanted caches
     e.waitUntil(
-        caches.keys().then(CACHE_NAMES => {
+        caches.keys(DATA_CACHE).then(cacheList => {
             return Promise.all(
-                CACHE_NAMES.map(cache => {
-                    if(cache !== CACHE_NAME) {
-                        console.log('Service Worker: Clearing Old Cache');
+                cacheList.map(cacheKey => {
+                    if (cacheKey !== CACHE_NAME && cacheKey !== DATA_CACHE) {
+                        caches.delete(cacheKey);
                     }
                 })
             )
-        })
+        }
+        )
     )
+    self.clients.claim();
 });
 
+self.addEventListener("fetch", (e) => {
+    if (e.request.url.inculdes("/api/")) {
+        e.respondWith(
+           caches.open(DATA_CACHE)
+           .then(cache => {
+               return fetch(e.request)
+                    .then(response => {
+                        //if it is goo it gets cloned and stores it in the cache
+                        if (response.status === 200) {
+                            cache.put(e.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(e.request);
+                    });
+            }).catch(err => console.log(err))
+        )
+        return;
+    }
+
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
